c6951e3ba7b93148ac2a8e4d9f6db7c1300a714d
adn87 <aaghadieh@gmail.com>
2023-11-23 08:06:44 -0500

    Selenium_Tutorial_005
    - created a new class for interacting with websites using selenium.

--- interaction.py
+++ interaction.py
@@ -1,14 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
+# driver.get("https://en.wikipedia.org/wiki/Main_Page")
+driver.get("https://secure-retreat-92358.herokuapp.com/")
 
-article_count = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
+# article_count = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
+# article_count.click()
 
-print(article_count.text)
+# Commons = driver.find_element(By.LINK_TEXT, value="Commons")
+# Commons.click()
 
+# search = driver.find_element(By.NAME, value="search")
+# search.send_keys("Python")
+# search.send_keys(Keys.ENTER)
+
+fname = driver.find_element(By.NAME, value="fName")
+fname.send_keys("George")
+lname = driver.find_element(By.NAME, value="lName")
+lname.send_keys("Washington")
+email = driver.find_element(By.NAME, value="email")
+email.send_keys("dummy@gmail.com")
+submit = driver.find_element(By.CSS_SELECTOR, value="form button")
+submit.click()
 driver.quit()
