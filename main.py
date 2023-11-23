1e7913631d2f6c0ffc0e20bb60b7eea7614c4939
adn87 <aaghadieh@gmail.com>
2023-11-23 03:26:50 -0500

    Selenium_tutorial_003
    - web scrapping using selenium webdriver to get elements using different methods. Then saving these elements in a dictionary.

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
 # Keep Chrome Browser open after program finishes
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 
 driver = webdriver.Chrome(options=chrome_options)
-# driver.get("https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6")
-driver.get("https://www.nintendo.com/us/store/products/the-legend-of-zelda-breath-of-the-wild-switch/")
-path = driver.find_element(By.XPATH, value='//*[@id="main"]/section[1]/div/div[3]/div[4]/div/div/span/text()')
-print(path.text)
-# price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole").text
-# price_cent = driver.find_element(By.CLASS_NAME, value="a-price-fraction").text
-# print(f"the price is: {price_dollar}.{price_cent}")
+driver.get("https://www.python.org/")
+
+time_events = driver.find_elements(By.CSS_SELECTOR, value=".medium-widget.event-widget.last > div > ul > li > time")
+time = [event.text for event in time_events]
+# print(time)
+
+title_events = driver.find_elements(By.CSS_SELECTOR, value=".medium-widget.event-widget.last  div  ul  li  a")
+title = [event.text for event in title_events]
+# print(title)
+
+events = {}
+for n in range(len(time_events)):
+    events[n] = {
+        "time": time_events[n].text,
+        "title": title_events[n].text
+    }
+print(events)
+
 
 
 
